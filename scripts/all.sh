3d3da6dd70a31a7c02c95810dac8cc457637c5bb
Chris Hamons <chris.hamons@xamarin.com>
2021-09-02 15:15:03 -0500

    More script work

--- scripts/all.sh
+++ scripts/all.sh
@@ -3,6 +3,6 @@
 for t in `ls ~/Programming/macios/master/xamarin-macios/tests/xtro-sharpie/MacCatalyst-*.todo`; do
 	name=`echo $t | grep -o '[^/]*$' | cut -d'.' -f1 | cut -d'-' -f2`
 	echo $name
-	dotnet run --no-restore --no-build -- -d=/Users/donblas/Programming/macios/master/xamarin-macios/tests/xtro-sharpie -v -s $name | grep 'Original\|Removed'
+	dotnet run --no-restore --no-build -- -d=/Users/donblas/Programming/macios/master/xamarin-macios/tests/xtro-sharpie -v -s $name | grep 'Orignal\|Removed'
 	echo 
 done
---
+++ scripts/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet run --no-restore --no-build -- -d=/Users/donblas/Programming/macios/master/xamarin-macios/tests/xtro-sharpie $@
